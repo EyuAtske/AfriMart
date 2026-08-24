8c2840d314791f0ce000b589b5f3cc73834f2309
Aynalem Sitotaw <aynalemsitotaw@gmail.com>
2026-08-24 17:27:03 +0300

    Add frontend project foundation

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+
+  css: ['~/assets/css/main.css'],
+  devtools: {
+    enabled: true
+  },
+
+  ssr: true,
+
+  vite: {
+    plugins: [
+      
+      tailwindcss()
+    ]
+  }
+})
