07cdd76ab53eb9e2400bc320ad8c68bdf006a47c
Aynalem Sitotaw <aynalemsitotaw@gmail.com>
2026-08-25 12:32:29 +0300

    fix: update gitignore, searchbar, package.json, and lockfile

--- frontend/app/components/layout/SearchBar.vue
+++ frontend/app/components/layout/SearchBar.vue
@@ -4,7 +4,7 @@
     <input
       type="text"
       placeholder="Search products or ★ ask AI..."
-      class="rounded-full border border-[#d7d0c4] px-8 py-3.5 text-sm w-10focus:outline-none"
+      class="rounded-full border border-[#d7d0c4] px-8 py-3.5 text-sm w-10 focus:outline-none"
     />
 
   </div>
