2499b646bf3f3746e37ccefcbf967b82d5e33dc6
EyuAtske <EyuelA.Fekade21@gmail.com>
2026-09-16 16:32:14 +0300

    Feat: wrote the order handler queries

---
+++ backend/internal/order/queries.go
@@ -0,0 +1,45 @@
+package order
+
+import (
+	"context"
+	"github.com/google/uuid"
+
+	"github.com/EyuAtske/AfriMart/backend/internal/database"
+)
+
+type OrderQuerier interface {
+	CreateOrder(
+		ctx context.Context,
+		arg database.CreateOrderParams,
+	) (database.Order, error)
+
+	CreateOrderItem(
+		ctx context.Context,
+		arg database.CreateOrderItemParams,
+	) (database.OrderItem, error)
+
+	GetOrderByID(
+		ctx context.Context,
+		arg database.GetOrderByIDParams,
+	) (database.Order, error)
+
+	GetOrderItems(
+		ctx context.Context,
+		orderID uuid.UUID,
+	)([]database.GetOrderItemsRow, error)
+
+	ListOrdersByUser(
+		ctx context.Context, 
+		arg database.ListOrdersByUserParams,
+	)([]database.Order, error)
+
+	ReduceProductStock(
+		ctx context.Context, 
+		arg database.ReduceProductStockParams,
+	)(database.ReduceProductStockParams, error)
+
+	UpdateOrderStatus(
+		ctx context.Context, 
+		arg database.UpdateOrderStatusParams,
+	)(database.Order, error)
+}
