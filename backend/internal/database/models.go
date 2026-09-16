da26cfb82f2ec37450427342f95243cde1e11598
EyuAtske <EyuelA.Fekade21@gmail.com>
2026-09-16 09:37:39 +0300

    Feat: generate the queries

--- backend/internal/database/models.go
+++ backend/internal/database/models.go
@@ -11,6 +11,22 @@ import (
 	"github.com/google/uuid"
 )
 
+type Cart struct {
+	ID        uuid.UUID
+	UserID    uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CartItem struct {
+	ID        uuid.UUID
+	CartID    uuid.UUID
+	ProductID uuid.UUID
+	Quantity  int32
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
 type Category struct {
 	ID        uuid.UUID
 	Name      string
